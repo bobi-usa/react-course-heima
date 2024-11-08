6a286cd5da69d8f73bbe7a3b5938c62762a8fb2d
bobi-usa <bobi_usa@163.com>
2024-11-08 11:28:01 +0800

    JSX中{}的应用：1.列表渲染2.条件渲染3.样式处理

--- src/index.js
+++ src/index.js
@@ -1,34 +1,82 @@
-// 1. 导包
-import React from 'react';
 import { createRoot } from 'react-dom/client';
+import './index.css';
 
-// 2. 创建 React 根对象
 const root = createRoot(document.querySelector('#root'));
 
-// 3. 渲染 React 内容
-// <h1>Hello React!</h1> => JSX（写在js中的HTML）
-root.render(
-  // JSX 的 3个规则：
-  // 1. 只能有一个根节点
-  // 2. 所有的标签必须闭合（像 img这种单标签，必须写 “/” 进行闭合）
-  // 3. 标签属性名称是驼峰命名法
-  <div>
-    <h1>Hello JSX!</h1>
-    <img
-      className="avatar"
-      src="https://avatars.githubusercontent.com/u/21986636?v=4"
-      alt="头像"
-    />
-  </div>
-);
+const categories = [
+  {
+    id: 1,
+    name: '推荐',
+  },
+  {
+    id: 2,
+    name: '一人套餐',
+  },
+  {
+    id: 3,
+    name: '西贝热菜',
+  },
+  {
+    id: 4,
+    name: '西贝凉菜',
+  },
+  {
+    id: 5,
+    name: '杂粮主食',
+  },
+];
 
-console.log(<h1 className="bbb">Hello World!</h1>);
-console.log(
-  React.createElement(
-    'h1',
-    {
-      className: 'aaa',
-    },
-    'Hello World!'
-  )
+/**
+ * JSX {} 的应用
+ * 写 {} 表示要写JS了，再写一个{}表示写的是JS对象，eg: style={ {} }，注意这并不是双括号语法
+ *
+ * 列表渲染
+ * 1. 数组的 map 方法
+ * 2. 列表项要添加 key 属性，key 值要唯一
+ *
+ * 条件渲染
+ * 1. 三元表达式 更适合：两个内容，要么渲染A，要么渲染B
+ * 2. 逻辑与 && 更适合：一个内容，渲染 or 不渲染
+ *
+ * 样式处理
+ * 1. className + 引入样式文件
+ * 2. 行内样式 style
+ */
+
+const selectedId = 3;
+
+root.render(
+  <div
+    style={{
+      width: '300px',
+      height: 500,
+      backgroundColor: 'blue',
+    }}
+  >
+    <ul className="list">
+      {categories.map(({ id, name }, index) => {
+        return (
+          <li
+            key={id}
+            className={id === selectedId ? 'list-item selected' : 'list-item'}
+          >
+            {/* {index === 0 ? <i>点</i> : ''} */}
+            {/* {index === 0 ? (
+              <img
+                src="https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/reactbase/waimai/menu-tuijian.png"
+                alt="推荐"
+              />
+            ) : null} */}
+            {index === 0 && (
+              <img
+                src="https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/reactbase/waimai/menu-tuijian.png"
+                alt="推荐"
+              />
+            )}
+            {name}
+          </li>
+        );
+      })}
+    </ul>
+  </div>
 );
