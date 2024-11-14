66cfd648577db112e8aa2c7cd8da5c57b213312c
bobi-usa <bobi_usa@163.com>
2024-11-14 10:23:52 +0800

    使用useRef进行DOM操作

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import { useState } from "react"
+import { useRef } from "react"
 
-// 使用状态操作表单元素的值
 const App = () => {
-  const [value, setValue] = useState('')
-  const [checked, setChecked] = useState(false)
+  const inputRef = useRef(null)
+
+  // 注意：不要在组件渲染时，使用 ref 进行 DOM 操作
+  console.log(inputRef.current)
+
   return (
     <div>
-      {/* 注意：value 和 onChange 需要同时出现 */}
-      <input type="text" value={value} onChange={e => setValue(e.target.value)} />
-      <button onClick={() => alert(value)}>获取</button>
-      <button onClick={() => { setValue('已修改') }}>修改</button>
-
+      <input ref={inputRef} type="text" />
       <hr />
-
-      <input
-        type="checkbox"
-        checked={checked}
-        onChange={e => setChecked(e.target.checked)}
-      />
-      {checked ? '选中了' : '未选中'}
+      <button onClick={() => console.log(inputRef.current.value)}>获取文本框的值</button>
+      <button onClick={() => inputRef.current.focus()}>获得焦点</button>
     </div>
   )
 }
