460bc9d19447ad2e3013329ded88ddc321550b64
bobi-usa <bobi_usa@163.com>
2024-11-15 17:16:30 +0800

    useEffect的使用

--- src/App.js
+++ src/App.js
@@ -1,94 +1,80 @@
-import { createContext, useContext, useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.scss'
 
-// 跨组件通讯 - Context
-// 1. 创建 Context 对象
-const ThemeContext = createContext()
-// 2. 划定范围，提供共享数据
-// 3. 范围内的组件，获取共享数据
+// useEffect 的使用
+/**
+ * 需求：在线聊天室的连接和关闭
+ * 步骤：
+ * 1. 挂载时，和默认聊天室建立连接
+ * 2. 更新时，和最新聊天室建立连接
+ * 3. 卸载时，和聊天室断开连接
+ */
 
-// -------------------------侧边栏-----------------------
-const Sidebar = () => {
-  return (
-    <div className="sidebar">
-      <Menu />
-    </div>
-  )
-}
-const Menu = () => {
-  return (
-    <div className="menu">
-      <ul>
-        <MenuItem />
-        <MenuItem />
-      </ul>
-    </div>
-  )
-}
-const MenuItem = () => {
-  // 3. 范围内的组件，获取共享数据
-  const { theme } = useContext(ThemeContext)
-  return <li style={{ color: theme }}>菜单</li>
-}
+// 聊天室
+const ChatRoom = ({ roomId }) => {
+  console.log('当前房间：', roomId)
 
-// -------------------------右侧内容-----------------------
-const Content = () => {
-  const { theme } = useContext(ThemeContext)
+  // 1. 挂载时，和默认聊天室建立连接
+  // 第二个参数默认空数组，就是默认挂载时执行，只执行一次
+  // 此阶段可以发送请求，进行 DOM 操作
+  // useEffect(() => {
+  //   console.log('建立连接：muisc 房间')
+  // }, [])
+
+  // 2. 更新时，和最新聊天室建立连接
+  // 注意：该调用方式会在组件挂载以及更新时都会执行
+  // 类似于生命周期钩子函数，但是并不是完全一样，比如现在看到的一进来就会执行一次，这个就不一样
+  // useEffect(() => {
+  //   console.log(`建立连接：${roomId} 房间`)
+  // }, [roomId])
+
+  // 3. 卸载时，和聊天室断开连接
+  // 卸载时，第二个参数也是空数组，空数组意味着这个 useEffect 只会走一次
+  useEffect(() => {
+    return () => {
+      console.log('断开连接')
+    }
+  }, [])
+
   return (
-    <div className="content">
-      <div className="main" style={{ color: theme }}>
-        Context 跨组件通讯
-      </div>
-      <Footer />
-    </div>
-  )
-}
-const Footer = () => {
-  const { onReset } = useContext(ThemeContext)
-  return (
-    <div className="footer">
-      <button onClick={onReset}>重置主题</button>
+    <div className="chat-room">
+      <h1>欢迎来到 {roomId} 房间</h1>
     </div>
   )
 }
 
-// 父组件
 const App = () => {
-  // 要共享的主题颜色
-  const [theme, setTheme] = useState('#1677FF')
-
-  // 重置主题的函数
-  const onReset = () => {
-    setTheme('#1677FF')
-  }
+  // 房间id
+  const [roomId, setRoomId] = useState('music')
+  // 是否正在聊天
+  const [chatting, setChatting] = useState(true)
 
   return (
-    <div className="app">
-      {/* 2. 划定范围，提供共享数据 */}
-      {/* 必须有value值，value值为要共享的数据 */}
-      <ThemeContext.Provider
-        value={{
-          theme,
-          onReset,
-          // ...
-        }}
+    <div className="App">
+      <button
+        onClick={() => setChatting(!chatting)}
       >
-        {/* 默认颜色： #1677FF */}
-        < input
-          className="theme-selector"
-          type="color"
-          value={theme}
-          onChange={e => setTheme(e.target.value)}
-        />
+        {chatting ? '退出聊天' : '开始聊天'}
+      </button>
 
-        <div className="main">
-          {/* 侧边栏 */}
-          <Sidebar />
-          {/* 右侧内容 */}
-          <Content />
-        </div>
-      </ThemeContext.Provider>
-    </div >
+      {
+        chatting ? (
+          <div>
+            <label>
+              选择聊天室
+              <select value={roomId} onChange={e => { setRoomId(e.target.value) }}>
+                <option value="music">music</option>
+                <option value="travel">travel</option>
+                <option value="sports">sports</option>
+              </select>
+            </label>
+            {/* 聊天室 */}
+            <ChatRoom roomId={roomId} />
+          </div>
+        ) : (
+          <p>点击【开始聊天】按钮，开始吧～</p>
+        )}
+    </div>
   )
 }
 
